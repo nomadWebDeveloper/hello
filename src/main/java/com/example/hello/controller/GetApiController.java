a642045d6cfefb6e7b75c1c08296eb64a29bb6ee
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-04-05 20:43:59 +0900

    Query Parameter

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,10 +1,10 @@
 package com.example.hello.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.UserRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -28,4 +28,43 @@ public class GetApiController {
     }
 
     // http://localhost:8080/api/get/query=param?user=steve&email=steve@gmail.com&age=30
+
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach( entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("/n");
+
+            sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    @GetMapping(path = "query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ) {
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + " " + email + " " + age;
+    }
+
+    @GetMapping(path = "query-param03")
+    public String queryParam03(UserRequest userRequest) {
+
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
+
 }
